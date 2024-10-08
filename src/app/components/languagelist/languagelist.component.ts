95ba79843de7b93c81c411d4b381b946dcda8326
RodelDecio <rodeldecio068@gmail.com>
2024-10-08 23:40:48 +0800

    #16languagelistservices

--- src/app/components/languagelist/languagelist.component.ts
+++ src/app/components/languagelist/languagelist.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LanguageListService } from '../../services/languagelist/languagelist.service';
+import { Language } from '../../interface/language';
 
 @Component({
   selector: 'app-languagelist',
@@ -6,17 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./languagelist.component.css']
 })
 export class LanguageListComponent {
-  languageList: string[] = ['JavaScript', 'Python', 'Java', 'C++'];
-  language: string = '';
+  languages: Language[] = [];
+  name: string = '';
+  creator: string = '';
+  year: number | null = null;
+
+  constructor(private languageListService: LanguageListService) {
+    this.languages = this.languageListService.getLanguages();
+  }
 
   addLanguage(): void {
-    if (this.language.trim()) {
-      this.languageList.push(this.language.trim());
-      this.language = '';
+    if (this.name && this.creator && this.year) {
+      this.languageListService.addLanguage(this.name, this.creator, this.year);
+      this.name = '';
+      this.creator = '';
+      this.year = null;
     }
   }
 
   removeLanguage(index: number): void {
-    this.languageList.splice(index, 1);
+    this.languageListService.removeLanguage(index);
+    this.languages = this.languageListService.getLanguages();
   }
 }
